e59025923efe04fdeecf555871c62b46903c084c
Muhannad Alhomsi <mohannad.alhomse@gmail.com>
2020-10-16 23:35:58 +0300

    New edit in skills section

--- js/app.js
+++ js/app.js
@@ -1,103 +1,243 @@
-$(document).ready(function(){
-    var controller = new ScrollMagic.Controller();
+$(document).ready(function () {
+  var controller = new ScrollMagic.Controller();
 
-    var animationSpeed = 0.75;
-    var animationTimingIn = Expo.easeIn;
-    var animationTimingOut = Expo.easeOut;
+  var animationSpeed = 0.75;
+  var animationTimingIn = Expo.easeIn;
+  var animationTimingOut = Expo.easeOut;
 
-    /**header */
-    var Header_TL = new TimelineMax({ paused: true });
+  /**header */
+  var Header_TL = new TimelineMax({ paused: true });
 
-    var header_section = $(".mu-bg-header");
-    var text_span_h = $(header_section).find(".mu-solid span");
-    var bg_text_h = $(header_section).find(".bg");
-    var text_h = $(header_section).find(".mu-text-h");
-    var button_h = $(header_section).find(".button-container-2");
-    
-    Header_TL.fromTo( header_section,animationSpeed,{y:-1500,opacity:0,ease: animationTimingIn},{y:0, opacity: 1, ease: animationTimingOut},"+=.7")
-    .fromTo(bg_text_h, animationSpeed,{width:"140px",opacity:1,ease:animationTimingIn},{width:"0px",opacity:0,ease:animationTimingIn},"+=.1")
-    .fromTo(text_span_h, animationSpeed,{opacity:0,ease:animationTimingIn},{width:"0px",opacity:1,ease:animationTimingIn},"-=.1")
-    .staggerFromTo(text_h,animationSpeed,{y:"120%" ,opacity:0, ease:animationTimingIn},{y:"0%" ,opacity:1, ease:animationTimingOut},0.25)
-    .fromTo(button_h,animationSpeed,{y:-50,opacity:0,ease:animationTimingIn},{y:0, opacity:1, ease:animationTimingOut},2.1);
-    Header_TL.play();
+  var header_section = $(".mu-bg-header");
+  var text_span_h = $(header_section).find(".mu-solid span");
+  var bg_text_h = $(header_section).find(".bg");
+  var text_h = $(header_section).find(".mu-text-h");
+  var button_h = $(header_section).find(".button-container-2");
 
-    /**service */
-    var Service_TL = new TimelineMax({paused: true});
+  Header_TL.fromTo(
+    header_section,
+    animationSpeed,
+    { y: -1500, opacity: 0, ease: animationTimingIn },
+    { y: 0, opacity: 1, ease: animationTimingOut },
+    "+=.7"
+  )
+    .fromTo(
+      bg_text_h,
+      animationSpeed,
+      { width: "140px", opacity: 1, ease: animationTimingIn },
+      { width: "0px", opacity: 0, ease: animationTimingIn },
+      "+=.1"
+    )
+    .fromTo(
+      text_span_h,
+      animationSpeed,
+      { opacity: 0, ease: animationTimingIn },
+      { width: "0px", opacity: 1, ease: animationTimingIn },
+      "-=.1"
+    )
+    .staggerFromTo(
+      text_h,
+      animationSpeed,
+      { y: "120%", opacity: 0, ease: animationTimingIn },
+      { y: "0%", opacity: 1, ease: animationTimingOut },
+      0.25
+    )
+    .fromTo(
+      button_h,
+      animationSpeed,
+      { y: -50, opacity: 0, ease: animationTimingIn },
+      { y: 0, opacity: 1, ease: animationTimingOut },
+      2.1
+    );
+  Header_TL.play();
 
-    var service_section = $(".serv-section")
-    var text_serv = $(service_section).find("h4");
-    var img_all_x = $(service_section).find(".mu-s-bg");
-    var img_sec_s = $(service_section).find(".mu-img-l");
+  /**service */
+  var Service_TL = new TimelineMax({ paused: true });
 
-    Service_TL.fromTo(service_section,animationSpeed,{x:"-100%",ease:animationTimingIn},{x:"0%",ease:animationTimingOut})
-    .fromTo(text_serv,animationSpeed,{opacity: 0,ease:animationTimingIn},{opacity: 1,ease:animationTimingOut})
-    .fromTo(img_all_x,animationSpeed,{x:"0%",ease:animationTimingIn},{x:"100%",display: "none",ease:animationTimingOut})
-    .fromTo(img_sec_s,animationSpeed,{x:"-100%",ease:animationTimingIn},{x:"100%", ease:animationTimingOut},"-=0.58");
-    /*Scene */
-    new ScrollMagic.Scene({
-        triggerElement: ".serv-section",
-        triggerHook: 0.75
-    })
+  var service_section = $(".serv-section");
+  var text_serv = $(service_section).find("h4");
+  var img_all_x = $(service_section).find(".mu-s-bg");
+  var img_sec_s = $(service_section).find(".mu-img-l");
+
+  Service_TL.fromTo(
+    service_section,
+    animationSpeed,
+    { x: "-100%", ease: animationTimingIn },
+    { x: "0%", ease: animationTimingOut }
+  )
+    .fromTo(
+      text_serv,
+      animationSpeed,
+      { opacity: 0, ease: animationTimingIn },
+      { opacity: 1, ease: animationTimingOut }
+    )
+    .fromTo(
+      img_all_x,
+      animationSpeed,
+      { x: "0%", ease: animationTimingIn },
+      { x: "100%", display: "none", ease: animationTimingOut }
+    )
+    .fromTo(
+      img_sec_s,
+      animationSpeed,
+      { x: "-100%", ease: animationTimingIn },
+      { x: "100%", ease: animationTimingOut },
+      "-=0.58"
+    );
+  /*Scene */
+  new ScrollMagic.Scene({
+    triggerElement: ".serv-section",
+    triggerHook: 0.75,
+  })
     .setTween(Service_TL)
     .reverse(true)
     .addTo(controller);
 
+  /**about */
+  var About_TL = new TimelineMax({ paused: true });
 
-    /**about */
-    var About_TL = new TimelineMax({paused: true});
+  var about_section = $(".about-section");
+  var title_about = $(about_section).find(".mu-header-title");
+  var para_about = $(about_section).find(".about-info p");
 
-    var about_section = $(".about-section");
-    var title_about = $(about_section).find(".mu-header-title");
-    var para_about = $(about_section).find(".about-info p");
+  About_TL.fromTo(
+    title_about,
+    animationSpeed,
+    { opacity: 0, x: "-100px", animationTimingIn },
+    { opacity: 1, x: "0px", animationTimingOut }
+  ).fromTo(
+    para_about,
+    animationSpeed,
+    { opacity: 0, x: "100px", animationTimingIn },
+    { opacity: 1, x: "0px", animationTimingOut },
+    0.2
+  );
 
-    About_TL.fromTo(title_about, animationSpeed ,{opacity:0, x:"-100px" ,animationTimingIn},{opacity:1, x:"0px", animationTimingOut})
-    .fromTo(para_about, animationSpeed ,{opacity:0, x:"100px" ,animationTimingIn},{opacity:1, x:"0px", animationTimingOut},.2)
-
-    /*Scene */
-    new ScrollMagic.Scene({
-        triggerElement: ".about-section",
-        triggerHook: 0.70
-    })
+  /*Scene */
+  new ScrollMagic.Scene({
+    triggerElement: ".about-section",
+    triggerHook: 0.7,
+  })
     .setTween(About_TL)
     .reverse(true)
     .addTo(controller);
 
+  /**skills */
+  /*new add*/
+  var skills_section = $(".skills-section");
+  var title_skills = $(skills_section).find(".mu-header-title");
 
-    /**skills */
-    var Skills_TL = new TimelineMax({paused: true});
+  var Skills_TL = new TimelineMax({ paused: true });
 
-    var skills_section = $(".skills-section");
-    var title_skills = $(skills_section).find(".mu-header-title");
-    var part1 = $(skills_section).find("#B");
-    var part2 = $(skills_section).find(".mu-hover-part");
+  Skills_TL.fromTo(
+    title_skills,
+    animationSpeed,
+    { opacity: 0, animationTimingIn },
+    { opacity: 1, animationTimingOut }
+  );
+  Skills_TL.from("#UiDesing", 4, {
+    y: -10,
+    x: -10,
+    opacity: 0,
+    ease: Power3.easeInOut,
+  })
+    .from(
+      "#HTML5",
+      4,
+      {
+        y: 100,
+        x: +400,
+        opacity: 0,
+        ease: Power3.easeInOut,
+      },
+      "-=4"
+    )
+    .from(
+      "#UIKIT",
+      4,
+      {
+        y: 230,
+        x: 340,
+        opacity: 0,
+        ease: Power3.easeInOut,
+      },
+      "-=4"
+    )
+    .from(
+      "#ReactJs",
+      4,
+      {
+        y: 300,
+        x: 200,
+        opacity: 0,
+        ease: Power3.easeInOut,
+      },
+      "-=4"
+    )
+    .from(
+      "#JavaScript",
+      4,
+      {
+        y: 200,
+        x: 10,
+        opacity: 0,
+        ease: Power3.easeInOut,
+      },
+      "-=4"
+    )
+    .from(
+      "#CSS3",
+      4,
+      {
+        y: 90,
+        x: 35,
+        opacity: 0,
+        ease: Power3.easeInOut,
+      },
+      "-=4"
+    );
 
-    Skills_TL.fromTo(title_skills, animationSpeed ,{opacity:0, x:"100px" ,animationTimingIn},{opacity:1, x:"0px", animationTimingOut})
-    .from(part1,animationSpeed, {opacity:0,y:"120%",animationTimingIn})
-    .staggerFromTo(part2,animationSpeed,{y:"120%" ,opacity:0, ease:animationTimingIn},{y:"0%" ,opacity:1, ease:animationTimingOut},0.25);
-
-    /*Scene */
-    new ScrollMagic.Scene({
-        triggerElement: ".skills-section",
-        triggerHook: 0.70
-    })
+  new ScrollMagic.Scene({
+    triggerElement: ".skills-section",
+    duration: "100%",
+    triggerHook: 0,
+  })
     .setTween(Skills_TL)
     .reverse(true)
+    .setPin(".skills-section")
     .addTo(controller);
 
+  //   var part1 = $(skills_section).find("#B");
+  //   var part2 = $(skills_section).find(".mu-hover-part");
 
-    /**project */
-    var Project_TL = new TimelineMax({paused: true})
+  //   /*Scene */
+  // new ScrollMagic.Scene({
+  //   triggerElement: ".skills-section",
+  //   triggerHook: 0.7,
+  // })
+  //   .setTween(Skills_TL)
+  //   .reverse(true)
+  //   .addTo(controller);
 
-    var project_section = $(".project-section");
-    var img_part = $(project_section).find(".proj-img-bg");
+  /**project */
+  var Project_TL = new TimelineMax({ paused: true });
 
-    Project_TL.staggerFromTo(img_part,animationSpeed,{y:"100%",animationTimingIn},{y:"0%",animationTimingOut},0.25);
+  var project_section = $(".project-section");
+  var img_part = $(project_section).find(".proj-img-bg");
 
-    /*Scene */
-    new ScrollMagic.Scene({
-        triggerElement: ".project-section",
-        triggerHook: 0.70
-    })
+  Project_TL.staggerFromTo(
+    img_part,
+    animationSpeed,
+    { y: "100%", animationTimingIn },
+    { y: "0%", animationTimingOut },
+    0.25
+  );
+
+  /*Scene */
+  new ScrollMagic.Scene({
+    triggerElement: ".project-section",
+    triggerHook: 0.7,
+  })
     .setTween(Project_TL)
     .reverse(true)
     .addTo(controller);
